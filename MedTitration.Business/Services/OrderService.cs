aa0433497963a12c0b23a5fe1e2f3c78add5f604
David McGuffey <dmcguffey94@gmail.com>
2023-09-27 21:04:12 -0500

    restructured  program

---
+++ MedTitration.Business/Services/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using MedTitration.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedTitration.Business.Services
+{
+    public class OrderService
+    {
+        public OrderService()
+        {
+                
+        }
+        public void GetOrder(Order order)
+        {
+/*            Console.WriteLine(" ------------------ ORDER INFORMATION ------------------ ");
+            Console.WriteLine("Enter medication name:");
+            order.medName = Console.ReadLine();
+            Console.WriteLine("Enter the ordered dosage: ");
+            order.upperDose = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("What unit of measurement?");
+            order.upperDoseUnit = Console.ReadLine();
+            Console.WriteLine("Now the lower number measurement:");
+            order.lowerDose = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Measured in: ");
+            order.lowerDoseUnit = Console.ReadLine();
+
+            Console.WriteLine($"{order.upperDose}{order.upperDoseUnit}/{order.lowerDose}{order.lowerDoseUnit}");
+            Console.WriteLine(" ------------------------------------------------------ ");*/
+        }
+    }
+}
