ba916defc824f7bcc250570a327f8778f711ebec
David McGuffey <dmcguffey94@gmail.com>
2023-10-14 09:37:07 -0500

    first test GiveDose in process

--- MedOrder/Models/Order.cs
+++ MedOrder/Models/Order.cs
@@ -8,5 +8,11 @@ namespace MedTitration.Models.Models
 {
     public class Order
     {
+        public string MedName { get; set; }
+        public decimal UpperDose { get; set; }
+        public string UpperDoseUnit { get; set; }
+        public decimal LowerDose { get; set; }
+        public string LowerDoseUnit { get;set; }
+        public int TimeHours { get; set; }
     }
 }
