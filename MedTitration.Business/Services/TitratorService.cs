73668a26c97721b0c47d8bc600c6cf91026734c9
David McGuffey <dmcguffey94@gmail.com>
2023-10-28 11:57:46 -0500

    pre-merge

--- MedTitration.Business/Services/TitratorService.cs
+++ MedTitration.Business/Services/TitratorService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MedTitration.Models.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,59 @@ using System.Threading.Tasks;
 
 namespace MedTitration.Business.Services
 {
-    internal class TitratorService
+    public class TitratorService
     {
-        /*public TitrateDose(decimal upperDose, string upperDoseUnit, decimal lowerDose, string lowerDoseUnit)
+        public TitratorService() { }
+        //for IV push medications
+        public string GiveDose(Order order, Med med)
         {
-            //receive order
-            //find the dose titration by dividing medication order with stored medication
-        }*/
+            if (order == null || order.UpperDose == 0 && order.LowerDose == 0)
+            {
+                return ("Order not provided.");
+            }
+            if (med == null || med.UpperDose == 0 && med.LowerDose == 0)
+            {
+                return ("Medication not stocked in supply. Unable to provide the dose.");
+            }
+            else
+            {
+                var CorrectDose = order.UpperDose / (med.UpperDose / med.LowerDose);
+
+                return ($"{CorrectDose} {order.Ml}");
+            }
+
+
+
+        }
+
+        public string TitrateDripML(Order order)
+        {
+            if (order == null || order.LowerDose == 0) 
+            {
+                return "Cannot provide rate.";
+            }
+
+            if (order.TimeHours == 0)
+            {
+                return "Bolus";
+            }
+            var MeasuredDose = order.LowerDose / order.TimeHours;
+            return ($"{Math.Round(MeasuredDose, 2)} ml/hr");
+
+        }
+
+        public string TitrateDripMg(Order order)
+        {
+            if (order == null || order.UpperDose == 0)
+            {
+                return "Cannot provide rate.";
+            }
+
+            if (order.TimeHours == 0)
+            {
+                return "Bolus";
+            }
+            return ($"{Math.Round((order.UpperDose / order.TimeHours), 2)} mg/hr");
+        }
     }
 }
